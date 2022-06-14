1e86204a561d85782e069cdd88f2efb7ff0542aa
fawei <50777393@qq.com>
2022-06-14 23:40:00 +0800

    react day2

---
+++ fawei-react-study/src/index.js
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client';
+import App from './01-base/01-class组件';
+
+
+const container = document.getElementById('root');
+createRoot(container).render(<App></App>);
