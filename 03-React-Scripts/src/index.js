dfb88d125af1b3757fba916816621d46fb08cba5
fawei <50777393@qq.com>
2022-07-04 20:48:31 +0800

    react study manually create react app

--- 03-React-Scripts/src/index.js
+++ 03-React-Scripts/src/index.js
@@ -1,1 +1,15 @@
 // react项目入口
+import ReactDOM from 'react-dom/client';
+
+const App = <div>
+    <h1>这是一个react项目</h1>
+    <p>
+        我终于有了第一个react项目
+    </p>
+</div>
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(App);
+
+//npx react-scripts run build
+//npx react-scripts start
