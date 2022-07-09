b0ef8320dc0748d6117ddac4684a43507029ead9
fawei <50777393@qq.com>
2022-07-09 20:11:04 +0800

    react state study

---
+++ 12-State/src/index.js
@@ -0,0 +1,7 @@
+import reactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const root = reactDOM.createRoot(document.querySelector("#root"));
+
+root.render(<App></App>);
