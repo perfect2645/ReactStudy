910f4c988efc5f85ab19fa787137c779f06c3f19
fawei <50777393@qq.com>
2022-07-09 21:39:04 +0800

    react study state

--- 001-react-scripts/src/App.js
+++ 001-react-scripts/src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import "./App.css";
 
 const App = () => {
-  return <div></div>;
+  const [counter, setCounter] = useState(2);
+
+  const addHandler = (event) => {
+    setCounter(counter + 1);
+  };
+
+  const minusHandler = (event) => {
+    setCounter(counter - 1);
+  };
+
+  return (
+    <div className="container">
+      <h1>{counter}</h1>
+      <button className="btn" onClick={minusHandler}>
+        -
+      </button>
+      <button className="btn addBtn" onClick={addHandler}>
+        +
+      </button>
+    </div>
+  );
 };
 
 export default App;
