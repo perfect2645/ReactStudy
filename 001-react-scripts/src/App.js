e8833fa68ea94c225c978185474b9e98fd5ac863
fawei <50777393@qq.com>
2022-07-09 23:32:16 +0800

    react study state plus

--- 001-react-scripts/src/App.js
+++ 001-react-scripts/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 const App = () => {
   const [counter, setCounter] = useState(2);
+  const [user, setUser] = useState({ userName: "fawei", age: 25 });
 
   const addHandler = (event) => {
     setCounter(counter + 1);
@@ -12,13 +13,56 @@ const App = () => {
     setCounter(counter - 1);
   };
 
+  const userHandler = () => {
+    /*  Solution 1 : Object.assign （浅复制）
+
+    const newUser = Object.assign({ test: "test" }, user);
+    newUser.userName = "fawei assign";
+    console.log(newUser); */
+    /* Solution 2 : 解构赋值（浅复制） */
+    const newUser = { ...user, userName: user.userName + counter };
+    setUser(newUser);
+  };
+
+  const addHandlerPlus = (event) => {
+    /* 测试连点，连点之后只触发了一次 */
+    /*     setTimeout(() => {
+      setCounter(counter + 1);
+      console.log(counter);
+    }, 1000); */
+
+    /* 解决方案:回调函数 */
+
+    // setTimeout(() => {
+    //   setCounter((previewCounter) => {
+    //     console.log(previewCounter);
+    //     return previewCounter + 1;
+    //   });
+    // }, 1000);
+
+    setTimeout(() => {
+      setCounter((previewCounter) => previewCounter + 1);
+    }, 1000);
+  };
+
   return (
     <div className="container">
-      <h1>{counter}</h1>
+      <h1>count:{counter}</h1>
+      <p>
+        user:{user.userName} - {user.age}
+      </p>
       <button className="btn" onClick={minusHandler}>
         -
       </button>
       <button className="btn addBtn" onClick={addHandler}>
+        +
+      </button>
+      <br />
+      <button className="btn userBtn" onClick={userHandler}>
+        Rename
+      </button>
+      <br />
+      <button className="btn addBtn" onClick={addHandlerPlus}>
         +
       </button>
     </div>
