d8c8e5ceeb2e4217bb887eedd6c765026ff2d776
Fawei Cui <perfect200@163.com>
2022-07-31 20:44:17 +0800

    logs form

--- fawei-react-study/src/components/logsForm/LogsForm.js
+++ fawei-react-study/src/components/logsForm/LogsForm.js
@@ -3,12 +3,19 @@ import './LogsForm.css';
 import Card from '../../UI/Card';
 
 const LogsForm = () => {
+
+
+    const dateChangedHandler = (event) => {
+
+    }
+
+
     return (
         <Card className='logs-form'>
             <form>
                 <article>
                     <label htmlFor="date">日期</label>
-                    <input type="date" id="date" />
+                    <input type="date" id="date" onChange={dateChangedHandler} />
                 </article>
                 <article>
                     <label htmlFor="desc">内容</label>
