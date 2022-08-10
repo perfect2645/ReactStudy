46830b7dc6917ea01db253025adc0b9a3bef5a4b
fawei <50777393@qq.com>
2022-08-10 17:25:40 +0800

    backdrop-root

--- fawei-react-study/src/UI/backDrop/Backdrop.js
+++ fawei-react-study/src/UI/backDrop/Backdrop.js
@@ -1,7 +1,14 @@
 import "./Backdrop.css";
+import ReactDOM from "react-dom";
+
+//获取backdrop-root
+const backdropRoot = document.querySelector("#backdrop-root");
 
 const Backdrop = (props) => {
-  return <div className="mask">{props.children}</div>;
+  return ReactDOM.createPortal(
+    <div className="mask">{props.children}</div>,
+    backdropRoot
+  );
 };
 
 export default Backdrop;
