3186ae8213e94b9b14f53eba421f7304523aa505
Fawei Cui <perfect200@163.com>
2022-09-06 19:06:00 +0800

    use fontawesome

--- fawei-react-study/react-app/src/components/UI/counter/Counter.js
+++ fawei-react-study/react-app/src/components/UI/counter/Counter.js
@@ -1,11 +1,15 @@
 import classes from "./Counter.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const Counter = (props) => {
   return (
     <div className={classes.counter}>
       {props.amount && props.amount > 0 ? (
         <>
-          <button className={`${classes.btn} ${classes.sub}`}>-</button>
+          <button className={`${classes.btn} ${classes.sub}`}>
+            <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+          </button>
           <span className={classes.countNum}>{props.amount}</span>
         </>
       ) : null}
