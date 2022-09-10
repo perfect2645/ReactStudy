952907825aed918beb06e8c6a2fec09431a5b110
fawei <50777393@qq.com>
2022-09-10 22:03:14 +0800

    cart context

--- fawei-react-study/react-app/src/components/Cart/Cart.js
+++ fawei-react-study/react-app/src/components/Cart/Cart.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./Cart.module.css";
 import bagIcon from "../../asset/bag.png";
+import CartContext from "../../store/cart-context";
 
 const Cart = () => {
+  const ctx = useContext(CartContext);
+
   return (
     <div className={classes.cartContainer}>
       <div className={classes.cartIcon}>
         <img src={bagIcon} alt="" className={classes.cartBox} />
-        <div className={classes.amount}>2</div>
+        <div className={classes.amount}>{ctx.totalAmount}</div>
       </div>
-      <h3 className={classes.price}>12</h3>
+      <h3 className={classes.price}>{ctx.totalPrice}</h3>
       <button className={classes.settleBtn}>去结算</button>
     </div>
   );
