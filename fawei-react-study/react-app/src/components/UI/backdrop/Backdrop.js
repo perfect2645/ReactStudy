2d726a8830dad332c3962996dd9240fab81affb0
Fawei Cui <perfect200@163.com>
2022-09-11 16:48:36 +0800

    66 - backdrop

---
+++ fawei-react-study/react-app/src/components/UI/backdrop/Backdrop.js
@@ -0,0 +1,15 @@
+import classes from "./Backdrop.module.css";
+import ReactDOM from "react-dom";
+
+const backdropRoot = document.querySelector("#backdrop-root");
+
+const Backdrop = (props) => {
+  return ReactDOM.createPortal(
+    <div classes={`${classes.Backdrop} ${props.className}`}>
+      {props.children}
+    </div>,
+    backdropRoot
+  );
+};
+
+export default Backdrop;
