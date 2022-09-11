edb71b1017f104ab2e687c31c35d104684beb41d
Fawei Cui <perfect200@163.com>
2022-09-11 19:04:58 +0800

    cart details style

--- fawei-react-study/react-app/src/components/Cart/CartDetails.js
+++ fawei-react-study/react-app/src/components/Cart/CartDetails.js
@@ -1,12 +1,18 @@
 import Backdrop from "../UI/backdrop/Backdrop";
-import classes from "./Cart.module.css";
+import classes from "./CartDetails.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const CartDetails = () => {
   return (
     <Backdrop>
       <div className={classes.CartDetails}>
-        <header>
+        <header className={classes.header}>
           <h3 className={classes.title}>餐品详情</h3>
+          <div>
+            <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
+            <span>清空购物车</span>
+          </div>
         </header>
       </div>
     </Backdrop>
