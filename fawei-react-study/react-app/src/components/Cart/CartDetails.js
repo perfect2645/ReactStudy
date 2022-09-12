8bb892f50c1e2ac66e4e96593cfa055f5a122466
Fawei Cui <perfect200@163.com>
2022-09-12 15:27:17 +0800

    cartdetails style

--- fawei-react-study/react-app/src/components/Cart/CartDetails.js
+++ fawei-react-study/react-app/src/components/Cart/CartDetails.js
@@ -2,8 +2,13 @@ import Backdrop from "../UI/backdrop/Backdrop";
 import classes from "./CartDetails.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import CartContext from "../../store/cart-context";
+import { useContext } from "react";
+import Meal from "../meals/meal/Meal";
 
 const CartDetails = () => {
+  const ctx = useContext(CartContext);
+
   return (
     <Backdrop>
       <div className={classes.CartDetails}>
@@ -14,6 +19,12 @@ const CartDetails = () => {
             <span>清空购物车</span>
           </div>
         </header>
+
+        <div>
+          {ctx.items.map((meal) => (
+            <Meal key={meal.id} meal={meal}></Meal>
+          ))}
+        </div>
       </div>
     </Backdrop>
   );
