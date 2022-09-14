9abe3dde77210457d2e7053b6214eb417bc4a418
Fawei Cui <perfect200@163.com>
2022-09-14 19:33:38 +0800

    71 - confirmhandler

--- fawei-react-study/react-app/src/components/Cart/CartDetails.js
+++ fawei-react-study/react-app/src/components/Cart/CartDetails.js
@@ -3,18 +3,41 @@ import classes from "./CartDetails.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import CartContext from "../../store/cart-context";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Meal from "../meals/meal/Meal";
+import Confirm from "../UI/confirm/Confirm";
 
 const CartDetails = () => {
   const ctx = useContext(CartContext);
 
+  const [showConfirm, setShowConfirm] = useState(false);
+
+  const showConfirmHandler = () => {
+    setShowConfirm(true);
+  };
+
+  const cancelHandler = (e) => {
+    e.stopPropagation();
+
+    setShowConfirm(false);
+  };
+
+  const okHandler = () => {};
+
   return (
     <Backdrop>
+      {showConfirm && (
+        <Confirm
+          confirmMsg={"确认清空购物车吗"}
+          onCancel={cancelHandler}
+          onOk={okHandler}
+        ></Confirm>
+      )}
+
       <div className={classes.CartDetails} onClick={(e) => e.stopPropagation()}>
         <header className={classes.header}>
           <h3 className={classes.title}>餐品详情</h3>
-          <div className={classes.clear}>
+          <div className={classes.clear} onClick={showConfirmHandler}>
             <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
             <span>清空购物车</span>
           </div>
---
+++ fawei-react-study/react-app/src/components/UI/confirm/Confirm.js
@@ -0,0 +1,25 @@
+import classes from "./Confirm.module.css";
+import Backdrop from "../backdrop/Backdrop";
+
+const Confirm = (props) => {
+  return (
+    <Backdrop>
+      <div className={classes.confirmContainer}>
+        <p className={classes.confirmMsg}>{props.confirmMsg}</p>
+        <div>
+          <button
+            className={classes.cancelBtn}
+            onClick={(e) => props.onCancel(e)}
+          >
+            取消
+          </button>
+          <button className={classes.okBtn} onClick={(e) => props.onOk(e)}>
+            确认
+          </button>
+        </div>
+      </div>
+    </Backdrop>
+  );
+};
+
+export default Confirm;
