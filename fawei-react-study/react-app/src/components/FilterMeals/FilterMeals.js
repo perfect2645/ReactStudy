0bc80dae9ef26856a14e5344d6e97f10300dff6b
Fawei Cui <perfect200@163.com>
2022-10-03 19:03:01 +0800

    79 - clear effect

--- fawei-react-study/react-app/src/components/FilterMeals/FilterMeals.js
+++ fawei-react-study/react-app/src/components/FilterMeals/FilterMeals.js
@@ -1,28 +1,42 @@
-
 import classes from "./FilterMeals.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
 
 const FilterMeals = (props) => {
+  const [keyword, setKeyword] = useState("");
 
-    const searchTextChangeHandler = e => {
-        const keyWord = e.target.value.trim();
-        props.onFilter(keyWord);
-    }
+  const searchTextChangeHandler = (e) => {
+    setKeyword(e.target.value.trim());
+  };
 
-    return (
-        <div className={classes.FilterMeals}>
-            <div className={classes.inputOuter}>
-                <input type="text"
-                    placeholder={"请输入关键字"}
-                    className={classes.searchText}
-                    onChange={searchTextChangeHandler}
-                />
-                <FontAwesomeIcon icon={faSearch}
-                className={classes.searchIcon}></FontAwesomeIcon>
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      props.onFilter(keyword);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [keyword]);
+
+  return (
+    <div className={classes.FilterMeals}>
+      <div className={classes.inputOuter}>
+        <input
+          type="text"
+          value={keyword}
+          placeholder={"请输入关键字"}
+          className={classes.searchText}
+          onChange={searchTextChangeHandler}
+        />
+        <FontAwesomeIcon
+          icon={faSearch}
+          className={classes.searchIcon}
+        ></FontAwesomeIcon>
+      </div>
+    </div>
+  );
 };
 
 export default FilterMeals;
