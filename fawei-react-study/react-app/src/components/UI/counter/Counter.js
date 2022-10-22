c27712351d3ff044a5990bc972baee2a8a5e863f
fawei <50777393@qq.com>
2022-10-22 21:30:25 +0800

    counter

--- fawei-react-study/react-app/src/components/UI/counter/Counter.js
+++ fawei-react-study/react-app/src/components/UI/counter/Counter.js
@@ -6,23 +6,26 @@ import { useContext } from "react";
 import CartContext from "../../../store/cart-context";
 
 const Counter = (props) => {
-
   const ctx = useContext(CartContext);
 
   return (
     <div className={classes.counter}>
       {props.meal.amount && props.meal.amount > 0 ? (
         <>
-          <button className={`${classes.btn} ${classes.sub}`}
-            onClick={() => ctx.removeItem(props.meal)}>
+          <button
+            className={`${classes.btn} ${classes.sub}`}
+            onClick={() => ctx.removeItem(props.meal)}
+          >
             <FontAwesomeIcon icon={faMinus}></FontAwesomeIcon>
           </button>
           <span className={classes.countNum}>{props.meal.amount}</span>
         </>
       ) : null}
 
-      <button className={`${classes.btn} ${classes.add}`}
-        onClick={() => ctx.addItem(props.meal)}>
+      <button
+        className={`${classes.btn} ${classes.add}`}
+        onClick={() => ctx.addItem(props.meal)}
+      >
         <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
       </button>
     </div>
