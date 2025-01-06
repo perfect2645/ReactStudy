fc0b10f7482f36b21c0f5bbf3bedbd92702581d2
Fawei <50777393@qq.com>
2025-01-06 23:17:54 +0800

    react

--- fawei-react-study-2/react-app/src/index.js
+++ fawei-react-study-2/react-app/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import './import.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<React.StrictMode>aaa</React.StrictMode>);
