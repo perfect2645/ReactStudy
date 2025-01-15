9e9e4c8f7a254911e570600dfe581858a20c6867
Fawei <50777393@qq.com>
2025-01-15 22:23:35 +0800

    meal component style

--- fawei-react-study-2/react-app/src/components/Meals/Meal/Meal.js
+++ fawei-react-study-2/react-app/src/components/Meals/Meal/Meal.js
@@ -7,11 +7,11 @@ const Meal = () => {
       <div className={classes.imgBox}>
         <img src="/img/meals/1.png" alt="" />
       </div>
-      <div>
-        <h2>汉堡包</h2>
-        <p>百分百纯牛肉配搭爽脆酸瓜洋葱粒与美味番茄酱经典滋味让你无法抵挡！</p>
-        <div>
-          <span>12</span>
+      <div className={classes.content}>
+        <h2 className={classes.title}>汉堡包</h2>
+        <p className={classes.desc}>百分百纯牛肉配搭爽脆酸瓜洋葱粒与美味番茄酱经典滋味让你无法抵挡！</p>
+        <div className={classes.priceBox}>
+          <span className={classes.price}>12</span>
           <div>数量</div>
         </div>
       </div>
