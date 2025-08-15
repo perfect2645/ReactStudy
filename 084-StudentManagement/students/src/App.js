0df19b2b6d52f7f953178e12239df0873f1dd7be
perfect2645 <50777393@qq.com>
2025-08-15 23:42:30 +0800

    react - fetch data from server

--- 084-StudentManagement/students/src/App.js
+++ 084-StudentManagement/students/src/App.js
@@ -1,24 +1,26 @@
 import classes from "./App.module.css";
-import React from "react";
+import React, { useEffect } from "react";
 import StudentList from "./components/student/StudentList";
 
-const StudentData = [
-  {
-    id: 1,
-    attributes: { name: "孙悟空", age: 20, gender: "男", address: "花果山" },
-  },
-  {
-    id: 2,
-    attributes: { name: "猪八戒", age: 22, gender: "男", address: "高老庄" },
-  },
-  {
-    id: 3,
-    attributes: { name: "周瑠夏", age: 18, gender: "女", address: "二次元" },
-  },
-];
-
 function App() {
-  const [studtents, setStudents] = React.useState(StudentData);
+  const [studtents, setStudents] = React.useState([]);
+
+  useEffect(() => {
+    fetch("https://localhost:7023/api/student/all")
+      .then((response) => {
+        if (response.ok) {
+          response.json().then((data) => {
+            console.log(data);
+            setStudents(data.data || []);
+          });
+        } else {
+          console.error("Failed to fetch students");
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching students:", error);
+      });
+  }, []);
 
   return (
     <div className={classes.App}>
