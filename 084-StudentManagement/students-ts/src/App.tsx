201c8919c7593bc7fcf9a97ab267f218e9230903
perfect2645 <50777393@qq.com>
2025-09-01 00:18:07 +0800

    React - TS - Code clean

--- 084-StudentManagement/students-ts/src/App.tsx
+++ 084-StudentManagement/students-ts/src/App.tsx
@@ -1,17 +1,39 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useReducer, useState } from "react";
 import StudentList from "./components/student/StudentList";
 import classes from "./App.module.css";
 
-import type {
-  StudentInfo,
-  StudentAttribute,
-  StudentResponse,
-} from "./types/Students";
+import type { StudentInfo, StudentResponse } from "./types/Students";
+
+type StudentState = {
+  studentData: StudentInfo[];
+  loading: boolean;
+  error: any;
+};
+
+type Action =
+  | { type: "StartLoading" }
+  | { type: "Loaded"; payload: StudentInfo[] }
+  | { type: "Error"; payload: string };
+
+const studentReducer = (state: StudentState, action: Action): StudentState => {
+  switch (action.type) {
+    case "StartLoading":
+      return { ...state, loading: true, error: null };
+    case "Loaded":
+      return { studentData: action.payload, loading: false, error: null };
+    case "Error":
+      return { ...state, error: action.payload, loading: false };
+    default:
+      return state;
+  }
+};
 
 const App = () => {
-  const [students, setStudents] = useState<StudentInfo[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<any>(null);
+  const [students, studentDispatch] = useReducer(studentReducer, {
+    studentData: [],
+    loading: false,
+    error: null,
+  });
 
   useEffect(() => {
     fetchStudents();
@@ -19,10 +41,9 @@ const App = () => {
 
   const fetchStudents = useCallback(async () => {
     try {
-      setError(null);
-      setLoading(true);
+      studentDispatch({ type: "StartLoading" });
 
-      const response = await fetch("https://localhost:7023/api/student/all");
+      const response = await fetch("https://localhost:7023/api/student/all1");
 
       if (!response.ok) {
         const errorMsg = `Failed to fetch students: ${response.status} ${response.statusText}`;
@@ -30,19 +51,19 @@ const App = () => {
       }
 
       const data: StudentResponse = await response.json();
-      setStudents(data.data);
+      studentDispatch({ type: "Loaded", payload: data.data });
     } catch (err: any) {
-      setError(err);
-    } finally {
-      setLoading(false);
+      studentDispatch({ type: "Error", payload: err });
     }
   }, []);
 
   return (
     <div className={classes.app}>
-      {loading && <header>Loading...</header>}
-      {!loading && !error && <StudentList students={students}></StudentList>}
-      {error && <footer>{error.message}</footer>}
+      {students.loading && <header>Loading...</header>}
+      {!students.loading && !students.error && (
+        <StudentList students={students.studentData}></StudentList>
+      )}
+      {students.error && <footer>{students.error.message}</footer>}
     </div>
   );
 };
